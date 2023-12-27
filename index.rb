86cbca50ead35249a27cc734cf993a44a43567d1
Jayeon Hwang <jayeon512@gmail.com>
2023-12-27 15:14:26 -0600

    call api and write condition

--- index.rb
+++ index.rb
@@ -1,6 +1,5 @@
 require "http"
 
-<<<<<<< HEAD
 puts "   _                       _                         
 _| |___ ___ ___ ___ ___ _| |___ ___ ___ ___ ___ ___ 
 | . | -_| . |  _| -_| -_| . |  _| -_|_ -|_ -| -_|  _|
@@ -10,20 +9,12 @@ _| |___ ___ ___ ___ ___ _| |___ ___ ___ ___ ___ ___
 puts "please enter your location:"
 location = gets.chomp
 
+
 data = HTTP.get("http://api.weatherstack.com/current?access_key=#{ENV['API_KEY']}&query= #{location}&units=f").parse
-=======
-
-# puts "please enter your location:"
-# location = gets.chomp
-location = 60016
-
-data = HTTP.get("http://api.weatherstack.com/current?access_key=#{ENV['API_KEY']}& query=#{location.to_i} unit=f").parse
->>>>>>> 3f43ffea988e6237d101dfaf116bd053aa16c6c6
-
 temp = data["current"]["temperature"]
 description = data["current"]["weather_descriptions"]
 current_hour = data["location"]["localtime"]
-<<<<<<< HEAD
+
 city_name = data["location"]["name"]
 
 puts city_name
@@ -56,19 +47,4 @@ else
   puts '👔 👖 🧥 🧦 🧣 🥾 👢 🧤'
 end
 
-
-
-=======
-
-
-if temp >= 90 
-  puts '👕 🩳'
-  puts description
-  puts current_hour
-else 
-  puts '🧣'
-end
-
-#https://weatherstack.com/documentation
->>>>>>> 3f43ffea988e6237d101dfaf116bd053aa16c6c6
  
